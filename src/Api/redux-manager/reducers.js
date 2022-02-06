1198bac9b76d6ee13885727a72d7136704544449
Blessedadudu <adududuke@gmail.com>
2022-02-06 01:49:24 +0100

    update on test

--- src/Api/redux-manager/reducers.js
+++ src/Api/redux-manager/reducers.js
@@ -9,22 +9,22 @@ import { ADD_TO_LIST, REMOVE_FROM_LIST, CLEAR_ALL_LIST, ADD_NEW_LIST } from './c
   export const userLists = ( state = initialStateList, action) => {
     if (action.type === ADD_TO_LIST) {
       console.log("calledddd", 'action.payloadaction.payload')
-      return { ...state, items: action.payload };
+      return { ...state, items: action.payload, reload: false };
     }
 
 
     if (action.type === ADD_NEW_LIST) {
       console.log(action.payload, 'action.payloadaction.payload')
-      return { ...state, items: [...state.items, action.payload] };
+      return { ...state, items: [...state.items, action.payload], reload: false  };
     }
 
 
     if (action.type === REMOVE_FROM_LIST) {
-      return { ...state, items: action.payload };
+      return { ...state, items: action.payload, reload: false  };
     }
 
     if (action.type === CLEAR_ALL_LIST) {
-      return { ...state, items: [] };
+      return { ...state, items: [], reload: false  };
     }
 
     return state;
